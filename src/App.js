666c73ccc21fe1d37f82fbb1a4c374ddb6bfc8ae
chiku46 <chiragchhabriamaster@gmail.com>
2021-02-10 09:37:54 +0530

    Commit handson3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {CalculateScore} from '../src/Components/CalculateScore'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CalculateScore Name={"Steve"}
+      School={"DNV Public School"}
+      total={284}
+      goal={3}
+      />
     </div>
   );
 }
